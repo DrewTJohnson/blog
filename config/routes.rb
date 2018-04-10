21ceb339faf9dde7ad4b948327dac1fe545f5f47
drewtjohnson <dtjohnson1995@gmail.com>
2018-04-10 12:06:58 -0400

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  resources :articles do
+    resources :comments
+  end
+  
+  get 'welcome/index'
+
+  root 'welcome#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
+Rails.application.routes.draw do
+  resources :articles
+
+  root 'welcome#index'
+end
